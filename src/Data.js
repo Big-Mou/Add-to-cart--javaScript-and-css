2f66f2a6bffc624250157da3ed250f83e98f4be1
Mohamed Abdeltawab <115569012+Big-Mou@users.noreply.github.com>
2022-10-14 16:42:50 +0200

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,59 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Meal 1",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "assets/img/plate1.png",
+    },
+      
+    {
+      id: "ioytrhndcv",
+      name: "Meal 2",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "assets/img/plate2.png",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "Meal 3",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "assets/img/plate3.png",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Meal 4",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "assets/img/plate1.png",
+    },
+    {
+      id: "thyfhcbci",
+      name: "Meal 5",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "assets/img/plate2.png",
+    },
+    {
+      id: "thyfhcbcx",
+      name: "Meal 6",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "assets/img/plate3.png",
+    },
+    {
+      id: "thyfhcbcz",
+      name: "Meal 7",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "assets/img/plate1.png",
+    },
+    {
+      id: "thyfhcbcq",
+      name: "Meal 8",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "assets/img/plate2.png",
+    },
+  ];
